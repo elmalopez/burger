582454a10f9ede09fc08c93d8567ae331cbc35eb
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2019-02-24 21:47:05 -0800

    included heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 // setup the code to connect Node to MySQL
 var mysql = require ("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8080,
+var connection;
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);}
+else{
+  connection = mysql.createConnection({
+  host: "localhost",
     user: "root",
     password: "",
-    database: "burgers_db"
+    database: "burgers_db"  
   });
-
-  app.listen(PORT, function() {
+};
+ app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
   
